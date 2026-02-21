be86f9e6f150b1f94cfa221b4d68233abe9f470b
Geetika <geet1kanwar@gmail.com>
2026-02-22 00:45:55 +0530

    Added Basic bot project

---
+++ Basic/bot.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import ollama
+
+st.set_page_config(page_title="Local Ollama Chatbot")
+
+st.title("Local Ollama Chatbot")
+inp = st.text_input("You: ", key="user")
+
+if st.button("Send"):
+    if inp.strip() != "":
+        with st.spinner("Thinking..."):
+            response = ollama.chat(
+                model="gemma:7b",
+                messages=[
+                    {"role": "user", "content": inp}
+                ]
+            )
+
+        st.markdown("### Response: ")
+        st.write(response["message"]["content"])
