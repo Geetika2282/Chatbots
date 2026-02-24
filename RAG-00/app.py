76157e6b7452d3a97b60941b063215664cc5589e
Geetika <geet1kanwar@gmail.com>
2026-02-24 17:43:45 +0530

    documents

--- RAG-00/app.py
+++ RAG-00/app.py
@@ -1,6 +1,8 @@
 import tempfile
+import lancedb
 import PyPDF2
 import re
+import ollama
 
 from PyPDF2 import PdfReader
 
@@ -53,15 +55,48 @@ def chunk_text(text, chunk_size=300):
 
 
 
-temp_file_path = readPDF("example.pdf")
+temp_file_path = readPDF("C:\\Users\\geetikak\\Documents\\GitHub\\Chatbots\\RAG-00\\example.pdf")
 print(f"Text saved to: {temp_file_path}")
 
 text = load_data(temp_file_path)
 print(text)
+chunks = chunk_text(text, 10)
+for k in range(len(chunks)):
+    print(f"{k}:{chunks[k]}\n")
 
-chunks = chunk_text(text, 1)
+
+print("=" * 80)
+print(chunks[6])
+print("=" * 80)
 print("+" * 80)
-print(len(chunks[0]))
-print("+" * 80)
-print(chunks)
-print("+" * 80)
+
+# Vector DB
+db = lancedb.connect("mydb")
+
+def get_embedding(text):
+    response = ollama.embeddings(
+        model="embeddinggemma",
+        prompt=text
+    )
+    return response['embedding']
+
+print(len(get_embedding("hello")))
+
+table = db.create__table(
+    "chunks",
+    data=[{
+        "Vector": get_embedding("test"),
+        "text": "test"
+    }],
+    mode="overwrite"
+)
+
+def smart_chunk_resume(text):
+    sections = re.split(r'\n[A-Z\s]{3,}\n', text)
+    chunks = []
+
+    for sec in sections:
+        clean = sec.strip()
+        if len(clean) > 50:
+            chunks.append(clean)
+            
